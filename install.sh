3c64205ccbcd371c2c8ebfe3b389af9d3cf2e2a2
skbdtl00 <skbdtl00@gmail.com>
2025-11-24 20:23:17 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ set -e
 
 export GITHUB_SOURCE="Production"
 export SCRIPT_RELEASE="canary"
-export GITHUB_BASE_URL="https://raw.githubusercontent.com/pelican-installer/pelican-installer"
+export GITHUB_BASE_URL=""
 
 LOG_PATH="/var/log/pelican-installer.log"
 
